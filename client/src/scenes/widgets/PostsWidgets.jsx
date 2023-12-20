73b6d79fdb747c604909c504ec2bd0643e542cf8
Felipe Guerreiro <felipeyg@gmail.com>
2023-12-19 22:10:40 -0300

    add Posts and Post Widgets

--- client/src/scenes/widgets/PostsWidgets.jsx
+++ client/src/scenes/widgets/PostsWidgets.jsx
@@ -18,10 +18,13 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   };
 
   const getUserPosts = async () => {
-    const response = await fetch(`http://localhost:3001/posts/${userId}/posts`, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const response = await fetch(
+      `http://localhost:3001/posts/${userId}/posts`, 
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
     const data = await response.json();
     dispatch(setPosts({ posts: data }));
   };
@@ -32,7 +35,40 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     } else {
       getPosts();
     }
-  })
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <>
+      {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <PostWidget 
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={firstName + " " + lastName}  
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}  
+            likes={likes}
+            comments={comments}
+          />
+
+        )
+      )}
+    </>  
+  )
 };
 
 export default PostsWidget;
